2d9cf8ebb51185732e6967f0ca22a4d166c9700d
SnowflakeIsABumbleBee <97166092+SnowflakeIsABumbleBee@users.noreply.github.com>
2025-04-19 19:50:06 +0200

    first commit

---
+++ src/main/java/com/HTW_Berlin/Basszeit/BasszeitApplication.java
@@ -0,0 +1,13 @@
+package com.HTW_Berlin.Basszeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasszeitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasszeitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/HTW_Berlin/Basszeit/BasszeitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HTW_Berlin.Basszeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasszeitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
