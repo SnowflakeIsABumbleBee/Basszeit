f97a67dbb72ce8c4f8cbfb096a21ac11b0581fd6
SnowflakeIsABumbleBee <97166092+SnowflakeIsABumbleBee@users.noreply.github.com>
2025-04-19 23:46:48 +0200

    dependencys

--- src/main/java/com/HTW_Berlin/Basszeit/Music.java
+++ src/main/java/com/HTW_Berlin/Basszeit/Music.java
@@ -4,9 +4,57 @@ import java.awt.Image;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@Entity
 public class Music {
 
-    String title;
-    String interpeten;
-    Image img;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String interpret;
+    private String imageUrl; // Link zum Bild
+
+    // Konstruktoren
+    public Music() {
+    }
+
+    public Music(String title, String interpret, String imageUrl) {
+        this.title = title;
+        this.interpret = interpret;
+        this.imageUrl = imageUrl;
+    }
+
+    // Getter und Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInterpret() {
+        return interpret;
+    }
+
+    public void setInterpret(String interpret) {
+        this.interpret = interpret;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
