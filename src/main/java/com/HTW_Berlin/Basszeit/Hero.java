7a9b7461f21303b6692f4f7fbbf9e34e25e015ad
SnowflakeIsABumbleBee <97166092+SnowflakeIsABumbleBee@users.noreply.github.com>
2025-04-19 21:21:02 +0200

    herotest

---
+++ src/main/java/com/HTW_Berlin/Basszeit/Hero.java
@@ -0,0 +1,14 @@
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Hero {
+
+    private String name;
+    private String affiliation;
+    private double heightInCm;
+}
