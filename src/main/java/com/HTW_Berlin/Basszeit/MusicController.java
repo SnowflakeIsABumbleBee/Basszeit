ac8c41e0e7eef831c504c27ebe69d13cf48096a7
tomto <tomasz.zdrojewski@student.htw-berlin.de>
2025-04-25 13:05:52 +0200

    MusicController

--- src/main/java/com/HTW_Berlin/Basszeit/MusicController.java
+++ src/main/java/com/HTW_Berlin/Basszeit/MusicController.java
@@ -1,16 +1,11 @@
 package com.HTW_Berlin.Basszeit;
 
-import java.awt.Image;
-
-import javax.swing.ImageIcon;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class BasszeitController {
+public class MusicController {
 
     @GetMapping(path = "/music")
     public ResponseEntity<Music> getMusic() {
