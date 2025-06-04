881332fa619b7c1fc89cb106310a255180c755a0
TomaszZdrojewski <121866056+TomaszZdrojewski@users.noreply.github.com>
2025-06-04 14:50:19 +0200

    Update MusicController.java

--- src/main/java/com/HTW_Berlin/Basszeit/MusicController.java
+++ src/main/java/com/HTW_Berlin/Basszeit/MusicController.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
-@RestController
+@CrossOrigin(origins = "*")@RestController
 public class MusicController {
 
     @GetMapping(path = "/music")
