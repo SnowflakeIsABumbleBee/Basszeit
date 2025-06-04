76af8086157a8f157a77ad0606eddc5701104e19
tomto <tomasz.zdrojewski@student.htw-berlin.de>
2025-06-04 13:06:47 +0200

    Music class patch + controller update

--- src/main/java/com/HTW_Berlin/Basszeit/Music.java
+++ src/main/java/com/HTW_Berlin/Basszeit/Music.java
@@ -1,54 +1,20 @@
 package com.HTW_Berlin.Basszeit;
 
-import java.awt.Image;
+import lombok.Getter;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
+@Getter
 public class Music {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     private String title;
-    private String interpret;// Link zum Bild
+    private String artist;
 
-    // Konstruktoren
-    public Music() {
-    }
-
-    public Music(String title, String interpret) {
-        this.title = title;
-        this.interpret = interpret;
-    }
-
-    // Getter und Setter
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
+    public Music(Long id, String title, String artist) {
         this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
         this.title = title;
-    }
+        this.artist = artist;    }
 
-    public String getInterpret() {
-        return interpret;
-    }
-
-    public void setInterpret(String interpret) {
-        this.interpret = interpret;
-    }
 }
+
+
--- src/main/java/com/HTW_Berlin/Basszeit/MusicController.java
+++ src/main/java/com/HTW_Berlin/Basszeit/MusicController.java
@@ -1,15 +1,21 @@
 package com.HTW_Berlin.Basszeit;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:5176")
+@RestController
 public class MusicController {
 
-    @GetMapping(path = "/songs")
-    public ResponseEntity<Music> getMusic() {
-        final Music music = new Music("Doggy", "Katja Krassavice");
-        return ResponseEntity.ok(music);
+    @GetMapping(path = "/music")
+    public ResponseEntity<List<Music>> getMusic() {
+        List<Music> musicList = List.of(
+                new Music(1L, "Doggy", "Katja Krassavice"),
+                new Music(2L, "Tomasz", "573059")
+        );
+        return ResponseEntity.ok(musicList);
     }
+
 }
